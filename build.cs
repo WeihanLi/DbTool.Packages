6abae2efa7647513a54364f3e68448bc84f76003
Weihan Li <weihanli@outlook.com>
2025-12-21 13:32:08 +0800

    feat: dotnet 10 and CI refine

---
+++ build.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the MIT license.
+
+#:package WeihanLi.Common@1.0.84
+
+using WeihanLi.Common.Helpers;
+
+string[] srcProjects = [ 
+    "./src/DbTool.Core/DbTool.Core.csproj",
+    "./src/DbTool.MySql/DbTool.MySql.csproj",
+    "./src/DbTool.PostgreSql/DbTool.PostgreSql.csproj",
+    "./src/DbTool.SqlServer/DbTool.SqlServer.csproj"
+];
+string[] testProjects = [ 
+    "./test/DbTool.Test/DbTool.Test.csproj"
+];
+
+await DotNetPackageBuildProcess
+    .Create(options => 
+    {
+        options.SolutionPath = "./DbTool.Packages.slnx";
+        options.SrcProjects = srcProjects;
+        options.TestProjects = testProjects;
+    })
+    .ExecuteAsync(args);
--- build.ps1
+++
@@ -1,9 +0,0 @@
-[string]$SCRIPT = '.\build.cake'
- 
-# Install cake.tool
-dotnet tool install --global cake.tool
-
-# Start Cake
-Write-Host "dotnet cake $SCRIPT $ARGS" -ForegroundColor GREEN
-
-dotnet cake $SCRIPT $ARGS
